be2e0f9bd533d85b452970974ea3da15f1322803
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-03-11 14:45:48 -0800

    Init routes and api structure
    All commented for now

---
+++ server/config/routes.js
@@ -0,0 +1,35 @@
+const auth = require('./auth');
+const utils = require('./utils.js');
+
+module.exports = (app) => {
+  // // Auth
+  // app.get('/auth/signedin');
+
+  // // FB
+  // app.get('/auth/facebook');
+  // app.get('/auth/facebook/callback');
+
+  // // User Profiles
+  // app.get('/api/users/');
+  // app.post('/api/users/');
+  // app.put('/api/users/');
+  // app.delete('/api/users/');
+
+  // // Events
+  // app.get('/api/events');
+  // app.post('/api/events');
+  // app.put('/api/events');
+  // app.delete('/api/events');
+
+  // // Friends
+  //   // Friends list
+  // app.get('/api/friends');
+  // app.post('/api/friends');
+  //   // Friend requests
+  // app.get('/api/friends/request');
+  // app.post('/api/friends/request');
+
+  // // Groups
+  // app.get('/api/friends/groups');
+  // app.post('/api/friends/groups');
+};
