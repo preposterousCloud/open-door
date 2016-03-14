ac480fc027deb95e5d035bba8d44f7c38202c657
Vince Cipriani <vince.cipriani@gmail.com>
2016-03-14 14:45:19 -0700

    add ability to invite user and groups when creating event

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,40 +1,57 @@
 const Sequelize = require('sequelize');
 
 module.exports = function Event(sequelizeInstance) {
-  const event = sequelizeInstance.define('Event', {
-    name: Sequelize.STRING,
-    startDateUtc: Sequelize.DATE,
-    endDateUtc: Sequelize.DATE,
-    addressStreet1: Sequelize.STRING,
-    addressStreet2: Sequelize.STRING,
-    city: Sequelize.STRING,
-    stateAbbrev: Sequelize.STRING,
-    postalCode: Sequelize.STRING,
-  }, {
-    classMethods: {
-      createEvent: function createEvent(eventObj) {
-        return this.create(eventObj)
-        .then((event) => {
-          return event.setHost_user(eventObj.hostUser)
-          .then(() => {return event;});
-        });
-      },
-      makeEvent: function makeEvent(hostUser, name, startDateUtc, endDateUtc, addressStreet1
-        , addressStreet2, city, stateAbbrev, postalCode) {
-        return {
-          hostUser,
-          name,
-          startDateUtc,
-          endDateUtc,
-          addressStreet1,
-          addressStreet2,
-          city,
-          stateAbbrev,
-          postalCode,
-        };
-      },
+  const seq = sequelizeInstance;
+  const event = sequelizeInstance.define('Event',
+    {
+      name: Sequelize.STRING,
+      startDateUtc: Sequelize.DATE,
+      endDateUtc: Sequelize.DATE,
+      addressStreet1: Sequelize.STRING,
+      addressStreet2: Sequelize.STRING,
+      city: Sequelize.STRING,
+      stateAbbrev: Sequelize.STRING,
+      postalCode: Sequelize.STRING,
     },
-  });
-
+    {
+      classMethods: {
+        createEvent: function createEvent(eventObj) {
+          return this.create(eventObj)
+          .then((event) => {
+            return event.setHostUser(eventObj.hostUser)
+            .then(() => {return event;});
+          })
+          .then((event) => {
+            // If we got any users add them to the event
+            let a;
+            let b;
+            if (eventObj.users) {
+              a = event.setUsers(eventObj.users);
+            }
+            // If we got any groups add them to the event
+            if (eventObj.hosts) {
+              b = event.setGroups(eventObj.hosts);
+            }
+            return Promise.all([a, b]).then(() => event);
+          });
+        },
+        makeEventTemplate: function makeEvent(hostUser, name, startDateUtc, endDateUtc
+        , addressStreet1, addressStreet2, city, stateAbbrev, postalCode, users, groups) {
+          return {
+            hostUser,
+            name,
+            startDateUtc,
+            endDateUtc,
+            addressStreet1,
+            addressStreet2,
+            city,
+            stateAbbrev,
+            postalCode,
+            users,
+            groups,
+          };
+        },
+      },
+    });
   return event;
 };
