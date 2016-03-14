8bdfc18c75440ec2ff08b74b3575b237fb180322
Elliot Plant <elliotaplant@gmail.com>
2016-03-14 11:11:24 -0700

    Add empty style object for navbar

--- native/styles/Feed/feedStyles.js
+++ native/styles/Feed/feedStyles.js
@@ -4,14 +4,21 @@ module.exports = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#EEE',
+    backgroundColor: '#AAA',
   },
   listView: {
     paddingTop: 20,
-    backgroundColor: '#ededed',
+    backgroundColor: '#FBA',
   },
   rightContainer: {
     flex: 1,
+  },
+  pageTitle: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#00F',
+  },
+  navBar: {
+    // enter some styles here
   },
 });
