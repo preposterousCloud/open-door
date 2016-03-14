4b201a2e4be258e39fdd921fb66f8c3451a0aea0
Elliot Plant <elliotaplant@gmail.com>
2016-03-14 15:47:58 -0700

    Add event details page modal

--- native/components/Event/EventDetails.js
+++ native/components/Event/EventDetails.js
@@ -1,23 +1,28 @@
-import styles from '../../styles/Feed/feedStyles.js';
+import styles from '../../styles/Event/eventStyles.js';
 import { reducer, store } from '../../sharedNative/reducers/reducers.js';
 import NavigationBar from 'react-native-navbar';
-import React, { View } from 'react-native';
+import React, { View, Text } from 'react-native';
 
-const rightButtonConfig = {
+const rightNavButton = {
   title: 'Next',
-  handler: () => alert('hello!'),
+  handler: () => console.log('hello!'),
 };
 
-const titleConfig = { title: 'Event Feed' };
-
-const FeedNavBar = () => (
+const EventDetails = (props) => (
   <View>
     <NavigationBar
-      title={titleConfig}
-      rightButton={rightButtonConfig}
+      title={{ title: 'Event Details' }}
+      rightButton={rightNavButton}
       style={styles.navBar}
     />
+    <Text>Title: {store.getState().focusEventDetails.title}</Text>
+    <Text>Address: {store.getState().focusEventDetails.address}</Text>
+    <Text>Host: {store.getState().focusEventDetails.host}</Text>
   </View>
 );
 
-module.exports = FeedNavBar;
+EventDetails.propTypes = {
+  route: React.PropTypes.object,
+};
+
+module.exports = EventDetails;
