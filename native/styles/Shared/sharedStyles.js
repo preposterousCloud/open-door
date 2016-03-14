fd1c4c517b6d2ee6505874e8f8ce59d070fb1ffc
Elliot Plant <elliotaplant@gmail.com>
2016-03-14 12:09:31 -0700

    Add stylesheets for upcoming components

---
+++ native/styles/Shared/sharedStyles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+module.exports = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#AAA',
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#FBA',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  navBar: {
+    // enter some styles here
+  },
+});
