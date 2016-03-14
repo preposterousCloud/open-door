b9c0b778d6693eeed3ab7fb95f0b7ea92b735772
Elliot Plant <elliotaplant@gmail.com>
2016-03-14 15:49:14 -0700

    Add navigation to example event details page

--- native/components/Feed/Feed.js
+++ native/components/Feed/Feed.js
@@ -1,6 +1,8 @@
 import styles from '../../styles/Feed/feedStyles.js';
 import FeedList from './FeedList.js';
 import FeedNavbar from '../Shared/NavBar.js';
+import EventDetails from '../Event/EventDetails.js';
+import { reducer, store } from '../../sharedNative/reducers/reducers.js';
 import NavigationBar from 'react-native-navbar';
 import React, {
   Text,
@@ -9,10 +11,30 @@ import React, {
   TouchableHighlight,
  } from 'react-native';
 
+const navToExampleEvent = () => {
+  store.dispatch({
+    type: 'SET_FOCUS_EVENT',
+    data: {
+      title: 'Polar Quest!',
+      address: '944 Market St., San Francisco, CA',
+      host: 'Old Greg',
+    },
+  });
+  store.getState().navigator.push({
+    component: EventDetails,
+  });
+};
+
+const rightNavButton = {
+  title: 'Derecha',
+  handler: navToExampleEvent,
+};
+
 const Feed = () => (
   <View style={styles.container}>
     <FeedNavbar
       title={ 'Event Feed' }
+      rightButton={rightNavButton}
     />
     <FeedList />
   </View>
