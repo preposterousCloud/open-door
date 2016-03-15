41a83222169719abc9f3120c8b934c01c49137aa
Vince Cipriani <vince.cipriani@gmail.com>
2016-03-14 17:29:16 -0700

    remove unused code

--- server/models/User.js
+++ server/models/User.js
@@ -2,8 +2,6 @@
  * Here we map out any User methods we need and return a new Pseudoclassical class
  * which extend the DB user.
  */
-
-const UserDbModel = require('../db/database').User;
 const Sequelize = require('sequelize');
 
 module.exports = function User(sequelizeInstance) {
