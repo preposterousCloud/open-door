eadfcdde48efba7bfe39a18fc7d7d9140101138b
Vince Cipriani <vince.cipriani@gmail.com>
2016-03-15 19:47:20 -0700

    remove code for mocking. No longer needed

--- native/components/Door/SetDoor.js
+++ native/components/Door/SetDoor.js
@@ -3,18 +3,8 @@ import { reducer, store } from '../../sharedNative/reducers/reducers.js';
 import NavBar from '../Shared/NavBar.js';
 import React, { View, Text } from 'react-native';
 
-const getDoorData = () => {
-  store.dispatch({
-    type: 'SET_FOCUS_EVENT',
-    data: {
-      user: 'Old Greg',
-      doorStatus: 'CLOSED',
-    },
-  });
-};
 
 const SetDoor = (props) => {
-  getDoorData();
   const leftNavButton = {
     title: '<',
     handler: props.swipeLeft,
@@ -25,8 +15,8 @@ const SetDoor = (props) => {
       title={ 'Event Details' }
       leftButton={leftNavButton}
     />
-    <Text>User: {store.getState().event.focusEventDetails.user}</Text>
-    <Text>Door Status: {store.getState().event.focusEventDetails.doorStatus}</Text>
+    <Text>User: {'static'}</Text>
+    <Text>Door Status: {'static'}</Text>
   </View>
   );
 };
