ccee13884be6ccf96f74ec6156ccd9c9f54b4d23
Elliot Plant <elliotaplant@gmail.com>
2016-03-15 21:41:04 -0700

    Alert user if login attempt fails

--- native/components/Auth/Login.js
+++ native/components/Auth/Login.js
@@ -1,19 +1,28 @@
 import Swiper from '../Shared/Swiper.js';
 import { reducer, store } from '../../sharedNative/reducers/reducers.js';
 import React, {
+  Alert,
   View,
   Text,
   TouchableOpacity,
+  TextInput,
  } from 'react-native';
 import styles from '../../styles/Auth/authStyles.js';
 import api from '../../sharedNative/utils/login.js';
 
 const buttonNav = (userName) => {
   store.dispatch(api.setUser(userName))
-  .then(() => {
-    store.getState().navigation.navigator.push({
-      component: Swiper,
-    });
+  .then((action) => {
+    if (action) {
+      store.getState().navigation.navigator.push({
+        component: Swiper,
+      });
+    } else {
+      Alert.alert(`${userName}`, 'not found', [
+        { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+        { text: 'Create', onPress: () => console.log('OK Pressed'), style: 'default' },
+      ]);
+    }
   });
 };
 
@@ -22,6 +31,14 @@ const loginWithUser2 = () => buttonNav('user2');
 const loginWithUser3 = () => buttonNav('user3');
 const loginWithUser4 = () => buttonNav('user4');
 const loginWithUser5 = () => buttonNav('user5');
+
+let userName;
+
+const updateUserName = newUserName => { userName = newUserName; };
+const loginWithUser = () => {
+  console.log(userName);
+  buttonNav(userName);
+};
 
 const Login = () => (
   <View style={styles.loginButton}>
@@ -37,6 +54,17 @@ const Login = () => (
     <TouchableOpacity onPress={loginWithUser5}>
       <Text>user5</Text>
     </TouchableOpacity>
+    <TextInput
+      autoCapitalize={'none'}
+      autoCorrect={false}
+      maxLength={16}
+      placeholder={'userName'}
+      value={userName}
+      style={styles.userInput}
+      returnKeyType={'go'}
+      onChangeText={updateUserName}
+      onSubmitEditing={loginWithUser}
+    />
   </View>
 );
 
