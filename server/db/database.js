869ae74338f86a0defee62233d2cde3cdd78c090
Elliot Plant <elliotaplant@gmail.com>
2016-03-16 16:16:47 -0700

    Add friends table to database

--- server/db/database.js
+++ server/db/database.js
@@ -33,6 +33,8 @@ const _InjectDBConfig = (params) => {
 
   const Event = require('../models/Event')(sequelize);
 
+  User.belongsToMany(User, { through: 'rel_user_friends' });
+
   User.belongsToMany(Group, { through: 'rel_user_group' });
   Group.belongsToMany(User, { through: 'rel_user_group' });
 
