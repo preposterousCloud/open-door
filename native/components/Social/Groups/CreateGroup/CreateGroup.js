c22af32fc495e24d23d3b503c28e6cb05dbc433e
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-03-17 14:25:33 -0700

    Add dispatch to getAllUsers action in Groups component and copy respective state to props

--- native/components/Social/Groups/CreateGroup/CreateGroup.js
+++ native/components/Social/Groups/CreateGroup/CreateGroup.js
@@ -6,21 +6,36 @@ import { reducer, store } from '../../../../sharedNative/reducers/reducers.js';
 import styles from '../../../../styles/Social/socialStyles.js';
 import NavBar from '../../../Shared/NavBar.js';
 import CreateGroupShowFriendsList from './CreateGroupShowFriendsList.js';
+import { getAllUsers } from '../../../../sharedNative/actions/actions.js';
+
+const allUsers = () => {
+  store.dispatch(getAllUsers())
+  .then((allUsers) => {
+    console.log('allUsers', allUsers)
+    return allUsers.map((user) => {
+      return {
+        id: user.id,
+        userName: user.userName,
+      }
+    })
+  })
+}
 
 const cancelNewGroup = () => {
   store.getState().navigation.navigator.jumpBack();
 };
 
 const CreateGroup = (props) => {
+  allUsers();
   const leftNavButton = {
     title: 'X',
     handler: cancelNewGroup,
   };
 
-  // CHANGE FOR API ROUTE
+  // CHANGE ONCE FRIENDS FEATURE IS IMPLEMENTED
   const CreateGroupShowFriendsListContainer = connect(state => {
     return {
-      groups: state.user.Groups,
+      users: state.allUsers,
     };
   })(CreateGroupShowFriendsList);
 
