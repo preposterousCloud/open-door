af94f0e100eeb6f58cd3bcb1efa2dee983ef68d9
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-03-17 14:26:40 -0700

    Temporarily display all users on createGroupsFriendsList (will later be replaced with friends)

--- native/components/Social/Groups/CreateGroup/CreateGroupShowFriendsList.js
+++ native/components/Social/Groups/CreateGroup/CreateGroupShowFriendsList.js
@@ -11,22 +11,26 @@ import { reducer, store } from '../../../../sharedNative/reducers/reducers.js';
 import styles from '../../../../styles/Social/socialStyles.js';
 import CreateGroupShowFriendsListRow from './CreateGroupShowFriendsListRow.js';
 
-const convertGroupsToDataSource = (groups) => {
-  groups = groups || [];
+const convertAllUsersToDataSource = (users) => {
+  console.log('CGSFL', users)
+  users = users || [];
   return (new ListView.DataSource(
       { rowHasChanged: (row1, row2) => row1 !== row2 }
-    ).cloneWithRows(groups.map(group => group.name))
+    ).cloneWithRows(users.map(user => user.userName))
   );
 };
 
-const CreateGroupShowFriendsList = (props) => (
-  <View style={styles.container}>
-    <ListView
-      dataSource={convertGroupsToDataSource(props.groups)}
-      renderRow={CreateGroupShowFriendsListRow}
-      style={styles.listView}
-    />
-  </View>
-);
+const CreateGroupShowFriendsList = (props) => {
+  console.log('PROPS', props)
+  return (
+    <View style={styles.container}>
+      <ListView
+        dataSource={convertAllUsersToDataSource(props.users)}
+        renderRow={CreateGroupShowFriendsListRow}
+        style={styles.listView}
+      />
+    </View>
+  );
+}
 
 module.exports = CreateGroupShowFriendsList;
