5e6dc41dc75562d6b639bb59d171bce55e6200a7
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-03-17 16:14:15 -0700

    Consolidate PR #90-#92

--- native/sharedNative/reducers/reducers.js
+++ native/sharedNative/reducers/reducers.js
@@ -2,7 +2,6 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import navigation from './subReducers/navigation.js';
 import { user, allUsers } from './subReducers/user.js';
-import { currentEvent } from './subReducers/event.js';
 const actions = require('../ActionTypes');
 
 const app = (state = { isLoading: false }, action) => {
@@ -19,7 +18,6 @@ const reducer = combineReducers({
   navigation,
   user,
   allUsers,
-  currentEvent,
 });
 
 const store = createStore(reducer, applyMiddleware(thunk));
--- native/sharedNative/utils/api.js
+++ native/sharedNative/utils/api.js
@@ -78,4 +78,3 @@ export const postUser = (userName) => {
   .then(validateBody)
   .catch(catchErr);
 };
-
