5e7704f8783fdfa59c0f1fd147278c516d98321e
Vince Cipriani <vince.cipriani@gmail.com>
2016-03-16 19:50:51 -0700

    move currentEvent to the user part of the state and fix search logic

--- native/sharedNative/reducers/reducers.js
+++ native/sharedNative/reducers/reducers.js
@@ -2,7 +2,6 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import navigation from './subReducers/navigation.js';
 import user from './subReducers/user.js';
-import { currentEvent } from './subReducers/event.js';
 const actions = require('../ActionTypes');
 
 const app = (state = { isLoading: false }, action) => {
@@ -18,7 +17,6 @@ const reducer = combineReducers({
   app,
   navigation,
   user,
-  currentEvent,
 });
 
 const store = createStore(reducer, applyMiddleware(thunk));
