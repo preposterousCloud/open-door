f75174d85104cb498d55ce3f0d7e883c1feda4fc
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-03-16 17:40:41 -0700

    Handle new group POST request and populate db

--- server/controllers/Group.js
+++ server/controllers/Group.js
@@ -1,18 +1,17 @@
+'use strict'
+
+const Sequelize = require('sequelize');
 const db = require('../db/database').db;
 
-const _mapGroup = (group) => {
-  return {
-    id: group.id,
-    name: group.name,
-  };
-};
-
 module.exports.createGroup = function createGroup(req, res) {
-  if (!req.body.groupName) {
+  if (!req.body.groupName || !req.body.members) {
     res.status(404).send('Make sure to include a user name and appropriate properties');
   } else {
     db.Group.create({ name: req.body.groupName })
-    .then((group) => res.json(_mapGroup(group)))
+    .then((group) => {
+      console.log('Group Updated: ', req.body)
+      group.addUsers(JSON.parse(req.body.members));
+    })
     .catch((err) => {
       console.error(err);
       res.status(500).send('Unknown server problem');
