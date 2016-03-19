5344f7e44737266b74bc479b13fc934473271507
Elliot Plant <elliotaplant@gmail.com>
2016-03-18 20:13:41 -0700

    Modularize list view component creation

--- native/components/Social/Friends/AddFriends.js
+++ native/components/Social/Friends/AddFriends.js
@@ -6,48 +6,26 @@ import styles from '../../../styles/Social/socialStyles.js';
 import feedStyles from '../../../styles/Feed/feedStyles.js';
 import friendsApi from '../../../sharedNative/utils/friends.js';
 import { getAllUsers } from '../../../sharedNative/actions/actions.js';
+import {
+  exitButton,
+  navTo,
+  enterButton,
+  arrayToDataSource,
+  cancelButton,
+  makeClickableRow,
+  UserList,
+  makeListContainer,
+} from '../../Shared/Misc.js';
 
 const AddFriends = (props) => {
-  // exit button
-  const leftNavButton = {
-    title: 'X',
-    handler: store.getState().navigation.navigator.jumpBack,
-  };
+  // Begin TextInput methods
   const something = () => {
     console.log('form submit!');
   };
 
-
   let userName;
   const updateUserName = newUserName => { userName = newUserName; };
 
-  // convertArrayToDatasource
-  const convertArrayToDatasource = (array, prop) => {
-    array = array || [];
-    if (prop) {
-      array = array.map(item => item[prop]);
-    }
-
-    return (new ListView.DataSource(
-        { rowHasChanged: (row1, row2) => row1 !== row2 }
-      ).cloneWithRows(array)
-    );
-  };
-
-
-  const getAllUsersArray = () => {
-    store.dispatch(getAllUsers())
-    .then((allUsers) => {
-      return allUsers.map((user) => {
-        return {
-          id: user.id,
-          userName: user.userName,
-        };
-      });
-    });
-  };
-
-  // cancelButton
   const cancelButton = {
     text: 'Cancel',
     onPress: () => console.log('Cancel Pressed'),
@@ -63,75 +41,38 @@ const AddFriends = (props) => {
       },
     ]);
   };
+  // End TextInput methods
 
-  const addFriend = (user) => {
-    alertRequestSent(user);
+  const getAllUsersArray = () => {
+    store.dispatch(getAllUsers())
+    .then((allUsers) => {
+      return allUsers.map((user) => {
+        return {
+          id: user.id,
+          userName: user.userName,
+        };
+      });
+    });
   };
-
-  // UserListRow(user, addFriend);
-  const AddFriendsListRow = (user) => {
-    const addThisFriend = addFriend.bind(null, user);
-
-    return (
-      <View>
-        <TouchableOpacity
-          onPress={addThisFriend}
-          style={styles.group}
-        >
-          <View style={styles.listEntryView}>
-              <Text>{user.userName}</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    );
-  };
-
-  // duplicate
-  const convertAllUsersToDataSource = (users) => {
-    users = users || [];
-    return (new ListView.DataSource(
-        { rowHasChanged: (row1, row2) => row1 !== row2 }
-      ).cloneWithRows(users)
-    );
-  };
-
-  const AddFriendsList = (props) => {
-    const friendIds = props.user.friends.map(friend => friend.id);
-    const friendableUsers = props.users.filter(
-      possibleFriend => (
-        friendIds.indexOf(possibleFriend.id) === -1 && possibleFriend.id !== props.user.id)
-    );
-    return (
-      <View style={styles.container}>
-        <ListView
-          dataSource={convertAllUsersToDataSource(friendableUsers)}
-          renderRow={AddFriendsListRow}
-          style={styles.listView}
-        />
-      </View>
-    );
-  };
-
-  AddFriendsList.propTypes = {
-    users: React.PropTypes.array,
-    user: React.PropTypes.object,
-  };
+  const allUsers = getAllUsersArray();
 
   const AddFriendsListContainer = connect(state => {
+    const filterIds = state.user.friends ?
+      state.user.friends.map(friend => friend.id).concat(state.user.id) : [];
+    const targetUsers = state.allUsers.filter(targetUser => (filterIds.indexOf(targetUser.id) < 0));
     return {
-      users: state.allUsers,
+      listComponent: UserList,
+      rowComponent: makeClickableRow(alertRequestSent),
+      listData: targetUsers,
       user: state.user,
     };
-  })(AddFriendsList);
-
-
-  const allUsers = getAllUsersArray();
+  })(UserList);
 
   return (
     <View>
       <NavBar
         title={ 'Add Friend' }
-        leftButton={leftNavButton}
+        leftButton={exitButton}
       />
       <TextInput
         autoCapitalize={'none'}
