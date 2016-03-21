d941fa1330a2bf7a1b91778fa247dc7a5ca07811
Elliot Plant <elliotaplant@gmail.com>
2016-03-21 16:07:30 -0700

    Add user list filtering to AddFriends

--- native/components/Social/Friends/AddFriends.js
+++ native/components/Social/Friends/AddFriends.js
@@ -2,10 +2,10 @@ import React, { View, Text, TouchableOpacity, TextInput, ListView, Alert } from 
 import { connect } from 'react-redux';
 import { reducer, store } from '../../../sharedNative/reducers/reducers.js';
 import NavBar from '../../Shared/NavBar.js';
+import FilterTextInput from '../../Shared/FilterTextInput.js';
 import styles from '../../../styles/Social/socialStyles.js';
 import feedStyles from '../../../styles/Feed/feedStyles.js';
 import friendsApi from '../../../sharedNative/utils/friends.js';
-import { setFilterText, clearFilterText } from '../../../sharedNative/actions/actions.js';
 import {
   exitButton,
   cancelButton,
@@ -14,37 +14,7 @@ import {
   getAllUsersArray,
 } from '../../Shared/Misc.js';
 
-class FilterTextInput extends React.Component {
-  componentDidMount() {
-    store.dispatch(clearFilterText());
-  }
-
-  filterUsers(text) {
-    store.dispatch(setFilterText(text));
-  }
-
-  render() {
-    return (
-      <View>
-        <TextInput
-          autoCapitalize={'none'}
-          autoCorrect={false}
-          maxLength={16}
-          placeholder={'userName'}
-          style={styles.userInput}
-          returnKeyType={'go'}
-          onChangeText={this.filterUsers}
-        />
-      </View>
-    );
-  }
-}
 const AddFriends = (props) => {
-  // Begin TextInput methods
-  const something = () => {
-    console.log('form submit!');
-  };
-
   const cancelButton = {
     text: 'Cancel',
     onPress: () => console.log('Cancel Pressed'),
@@ -61,14 +31,15 @@ const AddFriends = (props) => {
     ]);
   };
 
-  // End TextInput methods
   const allUsers = getAllUsersArray();
 
   const AddFriendsListContainer = connect(state => {
-    console.log('state\'s filter text:', state.filterText);
+    const re = new RegExp(state.filterText, 'ig');
     const filterIds = state.user.friends ?
       state.user.friends.map(friend => friend.id).concat(state.user.id) : [];
-    const targetUsers = state.allUsers.filter(targetUser => (filterIds.indexOf(targetUser.id) < 0));
+    const targetUsers = state.allUsers.filter(targetUser => (
+      filterIds.indexOf(targetUser.id) < 0 && targetUser.userName.match(re)
+    ));
     return {
       listComponent: UserList,
       rowComponent: makeClickableRow(alertRequestSent),
