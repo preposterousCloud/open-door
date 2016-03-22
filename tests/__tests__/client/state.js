44ef6045ec5a343bd13893ece6708c1192a00ded
Vince Cipriani <vince.cipriani@gmail.com>
2016-03-21 18:52:27 -0700

    update test to match recent changes

--- tests/__tests__/client/state.js
+++ tests/__tests__/client/state.js
@@ -63,8 +63,6 @@ describe('Data Integration Tests', () => {
   });
   
   it('Should keep track of toggled objects in pendingSelections', () => {  
-    expect(store.getState().app.pendingSelections).toEqual({});
-
     store.dispatch(actions.toggleItemSelectionInList(3, 'testEvent'));
     expect(store.getState().app.pendingSelections.testEvent[3]).toBe(true);
     store.dispatch(actions.toggleItemSelectionInList(3, 'testEvent'));
