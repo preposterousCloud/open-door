e3348b8da969b9c2a7aaceacd4480f680566cd5c
Vince Cipriani <vince.cipriani@gmail.com>
2016-03-21 18:56:02 -0700

    factor out logic into a popScene function

--- native/components/Shared/Misc.js
+++ native/components/Shared/Misc.js
@@ -20,13 +20,17 @@ LoadingWheel.propTypes = {
   isLoading: React.PropTypes.bool,
 };
 
-const exitButton = {
-  title: 'X',
-  handler: () => { store.getState().navigation.navigator.pop(); },
-};
-
 const navTo = (component, focus) => {
   store.getState().navigation.navigator.push({ component, focus });
+};
+
+const popScene = () => {
+  store.getState().navigation.navigator.pop();
+};
+
+const exitButton = {
+  title: 'X',
+  handler: popScene,
 };
 
 const enterButton = (component, focus) => ({
@@ -148,4 +152,5 @@ module.exports = {
   makeListContainer,
   LoadingWheel,
   getAllUsersArray,
+  popScene,
 };
