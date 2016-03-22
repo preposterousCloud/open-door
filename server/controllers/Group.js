7ffe9b4a4da63685b67c6e19a9a7063df502efca
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-03-21 18:17:43 -0700

    Create backend route to get all groups for a given user

--- server/controllers/Group.js
+++ server/controllers/Group.js
@@ -1,23 +1,56 @@
-'use strict'
+'use strict';
 
 const Sequelize = require('sequelize');
 const db = require('../db/database').db;
 
-module.exports.createGroup = function createGroup(req, res) {
+const _mapGroup = (group) => {
+  return {
+    groupId: group.id,
+    members: (group.Users.map(member => {
+      return {
+        id: member.id,
+        userName: member.userName,
+      };
+    })),
+  };
+};
+
+module.exports.createGroup = (req, res) => {
   if (!req.body.groupName || !req.body.members) {
     res.status(404).send('Make sure to include a user name and appropriate properties');
   } else {
     db.Group.create({ name: req.body.groupName })
     .then((group) => {
-      console.log('Group Updated: ', req.body)
+      console.log('Group Updated: ', req.body);
       group.addUsers(JSON.parse(req.body.members))
-      .then(function() {
-        res.json(group)
-      })
+      .then(() => {
+        res.json(group);
+      });
     })
     .catch((err) => {
       console.error(err);
       res.status(500).send('Unknown server problem');
     });
   }
+};
+
+module.exports.getGroups = (req, res) => {
+  db.User.findOne({
+    include: [{
+      model: db.Group,
+      include: [{
+        model: db.User,
+      }],
+    }],
+    where: {
+      id: +req.params.id,
+    },
+  })
+  .then((groups) => res.json(groups.Groups.map((group) => {
+    return _mapGroup(group);
+  })))
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Unknown server problem');
+  });
 };
