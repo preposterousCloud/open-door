b91fbefba1a1fd6eda15665b232bd82051051eb7
Elliot Plant <elliotaplant@gmail.com>
2016-03-22 20:40:33 -0700

    Add vibe picker component

---
+++ native/components/Door/VibePicker.js
@@ -0,0 +1,56 @@
+import React, { Text, TouchableOpacity, View, PickerIOS } from 'react-native';
+
+const PickerItemIOS = PickerIOS.item;
+
+const vibes = {
+  ball: {
+    name: 'Fancy Ball',
+  },
+  birthday: {
+    name: 'Birthday Party',
+  },
+  kick: {
+    name: 'Kick Back',
+  },
+  jam: {
+    name: 'Jam Session',
+  },
+  rager: {
+    name: 'Rager',
+  },
+  dino: {
+    name: 'Dinotopia',
+  },
+};
+
+class VibePicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vibe: 'jam',
+      vibePickerItems: Object.keys(vibes).map((vibe) => (
+        <PickerItemIOS
+          key={vibe.name}
+          value={vibe}
+          label={vibes[vibe].name}
+        />
+      )),
+    };
+  }
+
+  render() {
+    const changeValue = (vibe) => {
+      this.setState({ vibe });
+    };
+    return (
+      <PickerIOS
+        selectedValue={this.state.vibe}
+        onValueChange={changeValue}
+      >
+        {this.state.vibePickerItems}
+      </PickerIOS>
+    );
+  }
+}
+
+module.exports = VibePicker;
