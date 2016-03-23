4d97199862230337abb07c97e8c24f09c9bf9aca
Elliot Plant <elliotaplant@gmail.com>
2016-03-23 16:53:07 -0700

    Simplify EventSettings code

--- native/components/Door/EventSettings.js
+++ native/components/Door/EventSettings.js
@@ -2,7 +2,8 @@ import React, { Text, TouchableOpacity, View, Alert } from 'react-native';
 import { StyleSheet } from 'react-native';
 import { store } from '../../sharedNative/reducers/reducers.js';
 import { Button } from '../Shared/Button';
-import { exitButton, cancelButton, makeListContainer, navToFull, popScene } from '../Shared/Misc';
+import { backButton, cancelButtonNav, cancelButton,
+  navToFull, popScene, getTruthies } from '../Shared/Misc';
 import { GroupList, UserList } from '../Shared/StatefulSelectList';
 const actions = require('../../sharedNative/actions/actions');
 import NavBar from '../Shared/NavBar.js';
@@ -11,21 +12,13 @@ import styles2 from '../../styles/Door/doorStyles.js';
 import StyledTextInput from '../Shared/StyledTextInput.js';
 import socialStyles from '../../styles/Social/socialStyles.js';
 
-const InviteSelects = (props) => {
-  const title = props.route.type === 'groups' ? 'Invite Groups' : 'Invite Friends';
-  const List = props.route.type === 'groups' ? GroupList : UserList;
-  return (
-    <View>
-      <NavBar title={title} leftButton={{ handler: exitButton.handler, title: 'Back' }} />
-      <List inviteFunc={props.route.inviteFunc} />
-    </View>
-  );
-};
-InviteSelects.propTypes = {
-  route: React.PropTypes.object,
-};
-
-const getTruthies = (obj) => Object.keys(obj).filter(key => obj[key]).map(i => +i);
+const InviteSelects = (props) => (
+  <View>
+    <NavBar title={props.route.title} leftButton={backButton} />
+    <props.route.listComponent inviteFunc={props.route.inviteFunc} />
+  </View>
+);
+InviteSelects.propTypes = { route: React.PropTypes.object };
 
 class EventSettings extends React.Component {
   constructor(props) {
@@ -42,20 +35,14 @@ class EventSettings extends React.Component {
   }
   render() {
     const updateEvent = (update) => {
-      const updatedEvent = this.state.event;
-      updatedEvent[Object.keys(update)[0]] = update[Object.keys(update)[0]];
-      this.setState({ event: updatedEvent });
+      const event = this.state.event;
+      event[Object.keys(update)[0]] = update[Object.keys(update)[0]];
+      this.setState({ event });
     };
     const submitEvent = () => {
-      console.log('Submitted vibe:', this.state.event.vibe);
-      console.log('Submitted event name:', this.state.event.name);
-      console.log('Submitted event details:', this.state.event.details);
-      console.log('Invited Friends:', this.state.event.invitedFriends);
-      console.log('Invited Groups:', this.state.event.invitedGroups);
       if (!this.state.event.name) {
         Alert.alert('Your door needs a name!', '', [cancelButton]);
       } else {
-        console.log('creating event:', this.state.event);
         this.state.event.friends = getTruthies(this.state.event.invitedFriends);
         this.state.event.groups = getTruthies(this.state.event.invitedGroups);
         this.state.onSubmit(this.state.event);
@@ -64,7 +51,7 @@ class EventSettings extends React.Component {
     };
     const updateEventName = name => updateEvent({ name });
     const updateEventDetails = details => updateEvent({ details });
-    const changeVibe = (vibe) => updateEvent({ vibe });
+    const changeVibe = vibe => updateEvent({ vibe });
     const toggleInviteFriend = (friendId) => {
       const event = this.state.event;
       event.invitedFriends[friendId] = !event.invitedFriends[friendId];
@@ -72,7 +59,8 @@ class EventSettings extends React.Component {
     };
     const navToFriends = () => navToFull({
       component: InviteSelects,
-      type: 'friends',
+      title: 'Invite Friends',
+      listComponent: UserList,
       inviteFunc: toggleInviteFriend,
     });
     const toggleInviteGroup = (groupId) => {
@@ -82,37 +70,25 @@ class EventSettings extends React.Component {
     };
     const navToGroups = () => navToFull({
       component: InviteSelects,
-      type: 'groups',
+      title: 'Invite Groups',
+      listComponent: GroupList,
       inviteFunc: toggleInviteGroup,
     });
     return (
-      <View style={ socialStyles.container }>
-        <StyledTextInput
-          onChangeText={updateEventName}
-          placeholder={'Event Name'}
+      <View>
+        <NavBar title={'Edit Event'} leftButton={cancelButtonNav}
+          rightButton={{ title: 'Save', handler: submitEvent }}
         />
-        <StyledTextInput
-          onChangeText={updateEventDetails}
-          placeholder={'Description (optional)'}
-        />
-        <Button onClick = {submitEvent} text={'Save'} />
-        <Button onClick = {popScene} text={'Cancel'} />
-        <VibePicker
-          changeVibe={changeVibe}
-          initialVibe={'jam'}
-        />
-        <TouchableOpacity
-          onPress={navToFriends}
-          style={socialStyles.categoryButton}
-        >
+        <StyledTextInput onChangeText={updateEventName} placeholder={'Event Name'} />
+        <StyledTextInput onChangeText={updateEventDetails} placeholder={'Description (optional)'} />
+        <VibePicker changeVibe={changeVibe} initialVibe={'jam'} />
+        <TouchableOpacity onPress={navToFriends} style={socialStyles.categoryButton} >
           <Text>FRIENDS</Text>
         </TouchableOpacity>
-        <TouchableOpacity
-          onPress={navToGroups}
-          style={socialStyles.categoryButton}
-        >
+        <TouchableOpacity onPress={navToGroups} style={socialStyles.categoryButton} >
           <Text>GROUPS</Text>
         </TouchableOpacity>
+        <Text>Hi</Text>
       </View>
     );
   }
