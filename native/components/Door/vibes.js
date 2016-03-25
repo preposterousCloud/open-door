0dc9614b0efc52ee72a45ebfb55d21ed88266a83
Elliot Plant <elliotaplant@gmail.com>
2016-03-24 20:18:05 -0700

    Move vibes to separate file

---
+++ native/components/Door/vibes.js
@@ -0,0 +1,26 @@
+module.exports = {
+  ball: {
+    name: 'Fancy Ball',
+    src: 'http://gph.is/1fWbxdT',
+  },
+  birthday: {
+    name: 'Birthday Party',
+    src: 'http://gph.is/1fWbxdT',
+  },
+  kick: {
+    name: 'Kick Back',
+    src: 'http://gph.is/1fWbxdT',
+  },
+  jam: {
+    name: 'Jam Session',
+    src: 'http://gph.is/1fWbxdT',
+  },
+  rager: {
+    name: 'Rager',
+    src: 'http://gph.is/1etwtsZ',
+  },
+  dino: {
+    name: 'Dinotopia',
+    src: 'http://gph.is/1fWbxdT',
+  },
+};
