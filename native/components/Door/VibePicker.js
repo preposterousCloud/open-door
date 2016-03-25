9a00b2a4b0127a758f0e369341aca4d9dc50d61d
Vince Cipriani <vince.cipriani@gmail.com>
2016-03-25 12:17:04 -0700

    fix warning on vibepicker

--- native/components/Door/VibePicker.js
+++ native/components/Door/VibePicker.js
@@ -1,4 +1,4 @@
-import React, { Text, TouchableOpacity, View, PickerIOS, PickerItemIOS } from 'react-native';
+import React, { Text, TouchableOpacity, View, Picker } from 'react-native';
 
 const vibes = {
   ball: {
@@ -25,30 +25,32 @@ class VibePicker extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      vibe: props.initialVibe || 'jam',
-      changeVibe: props.changeVibe,
-      vibePickerItems: Object.keys(vibes).map((vibe, index) => (
-        <PickerItemIOS
-          key={index}
-          value={vibe}
-          label={vibes[vibe].name}
-        />
-      )),
+      selectedVibe: props.initialVibe || 'jam',
     };
+    this.changePickerValue = this.changePickerValue.bind(this);
   }
-
+  changePickerValue(vibe) {
+    this.setState({ selectedVibe: vibe });
+    this.props.changeVibe(vibes[vibe]);
+  }
+  // Picker doesn't really care what type of element we provide as the item.
+  // It manually iterates the children of Picker and creates its own children elements from them
   render() {
-    const changeValue = (vibe) => {
-      this.setState({ vibe });
-      this.state.changeVibe(vibes[vibe]);
-    };
     return (
-      <PickerIOS
-        selectedValue={this.state.vibe}
-        onValueChange={changeValue}
+      <Picker
+        selectedValue={this.state.selectedVibe}
+        onValueChange={this.changePickerValue}
       >
-        {this.state.vibePickerItems}
-      </PickerIOS>
+        {
+          Object.keys(vibes).map((vibe, index) => (
+            <Picker.Item
+              key={index}
+              value={vibe}
+              label={vibes[vibe].name}
+            />
+          ))
+        }
+      </Picker>
     );
   }
 }
