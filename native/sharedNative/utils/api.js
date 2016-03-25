2ca611af4376bab722455436bcd5df4e80a4b62d
Vince Cipriani <vince.cipriani@gmail.com>
2016-03-24 20:43:20 -0700

    remove error code

--- native/sharedNative/utils/api.js
+++ native/sharedNative/utils/api.js
@@ -7,7 +7,6 @@ const validateBody = res => {
   if (statusOK(res)) {
     return JSON.parse(res._bodyInit);
   }
-  console.log(res);
   throw new Error(`Error processing request: Message: ${res._bodyText}
     Url: ${res.url}
     status: ${res.status}`);
