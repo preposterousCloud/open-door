789a7d5b1d6e3970f9f38b9daef56e68545fe33d
Elliot Plant <elliotaplant@gmail.com>
2016-03-26 13:19:21 -0700

    Format profile to more vanilla ness

--- native/components/Profile/ProfileSettingsListRow.js
+++ native/components/Profile/ProfileSettingsListRow.js
@@ -5,21 +5,13 @@ import styles from '../../styles/Profile/profileStyles.js';
 const ProfileSettingsListRow = () => (
   <View>
     <View style={styles.listEntryView}>
-      <Text style={styles.group}>Name: {store.getState().user.userName}</Text>
+      <Text style={styles.group}>Username: {store.getState().user.userName}</Text>
     </View>
     <View style={styles.listEntryView}>
-      <Text style={styles.group}>Nickname: TODO</Text>
-    </View>
-    <View style={styles.listEntryView}>
-      <Text style={styles.group}>Address: TODO</Text>
+      <Text style={styles.group}>Location: {store.getState().user.location}</Text>
     </View>
     <View style={styles.listEntryViewDef}>
-      <View style={styles.pullLeft}>
-        <Text style={styles.group}>Defaults</Text>
-      </View>
-      <View style={styles.pullRight}>
-        <Text style={styles.group}>></Text>
-      </View>
+      <Text style={styles.group}>Default Vibe: {store.getState().user.vibe}</Text>
     </View>
   </View>
 );
