fdff5e2d36a60a52829e91e8bd0cb6fcfcca9adc
Vince Cipriani <vince.cipriani@gmail.com>
2016-03-28 16:14:42 -0700

    Add base64 encoding for images

--- native/components/Profile/SelectProfilePic.js
+++ native/components/Profile/SelectProfilePic.js
@@ -1,16 +1,17 @@
-import React, { View, Text, Alert, TouchableOpacity } from 'react-native';
+import React, { View, Text, TouchableOpacity, NativeModules } from 'react-native';
 import { store } from '../../sharedNative/reducers/reducers.js';
 import NavBar from '../Shared/NavBar.js';
-import EditUser from './EditUser';
 import styles from '../../styles/Profile/profileStyles.js';
-import { backButton, editButton, cancelButtonNav } from '../Shared/Buttons';
+import { cancelButtonNav } from '../Shared/Buttons';
 import { navToFull } from '../Shared/NavHelpers';
 const CameraRollView = require('../Camera/CameraRollView');
 const actions = require('../../sharedNative/actions/actions');
 
 const SelectProfilePic = class SelectProfilePic extends React.Component {
   onPhotoSelection(imageObj) {
-    console.log(imageObj);
+    NativeModules.ReadImageData.readImage(imageObj.node.image.uri, (image) => {
+      console.log(image);
+    });
   }
   render() {
     return (
