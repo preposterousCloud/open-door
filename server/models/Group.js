260c023b280be086cbdd6d9b21b93f04b153ed89
Elliot Plant <elliotaplant@gmail.com>
2016-03-28 20:52:09 -0700

    Change name of group picture property

--- server/models/Group.js
+++ server/models/Group.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 module.exports = function Group(sequelizeInstance) {
   return sequelizeInstance.define('Group', {
     name: Sequelize.STRING,
-    picture: {
+    groupPictureUri: {
       type: Sequelize.String,
       defaultValue: 'http://img3.wikia.nocookie.net/__cb20140120043524/scratchpad/images/9/92/Power_Rangers_Megaforce_(Nick).jpg',
     },
