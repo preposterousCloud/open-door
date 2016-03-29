a07ace8bb2ef07e5be3e762cb7f7f57dd5075b13
Vince Cipriani <vince.cipriani@gmail.com>
2016-03-29 12:18:26 -0700

    enable CirlclePic to accept source like a normal Image

--- native/components/Shared/CirclePic.js
+++ native/components/Shared/CirclePic.js
@@ -1,14 +1,16 @@
 import React, { View, Image } from 'react-native';
 import styles from '../../styles/Profile/profileStyles.js';
 
-const CirclePic = (props) => (
-  <View>
-    <Image
-      source={{ uri: props.uri }}
-      style={styles.profilePic}
-    />
-  </View>
-);
+const CirclePic = (props) => {
+  return (
+    <View>
+      <Image
+        source={{ uri: props.source.uri }}
+        style={styles.profilePic}
+      />
+    </View>
+  );
+};
 
-CirclePic.propTypes = { uri: React.PropTypes.string };
+CirclePic.propTypes = { source: React.PropTypes.objects };
 module.exports = CirclePic;
