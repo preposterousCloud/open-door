e85507b371764a88255450ddcebb7bb7f0957c29
Elliot Plant <elliotaplant@gmail.com>
2016-03-30 16:38:33 -0700

    Merging

--- native/components/Feed/EventDetail.js
+++ native/components/Feed/EventDetail.js
@@ -5,6 +5,7 @@ import * as api from '../../sharedNative/utils/api.js';
 import { store } from '../../sharedNative/reducers/reducers.js';
 import vibes from '../Door/vibes.js';
 import CirclePic from '../Shared/CirclePic';
+import Swiper from 'react-native-swiper';
 
 const { width, height } = Dimensions.get('window');
 
@@ -14,6 +15,7 @@ class EventDetail extends React.Component {
     this.state = {
       imageShowing: props.imageShowing,
       imageSource: require('../../sharedNative/images/dino-storm.jpg'),
+      event: { Users: [], Groups: [] },
     };
     this.contactMapper = store.getState().contactMap;
     this.defaultMargin = 10;
@@ -62,41 +64,36 @@ class EventDetail extends React.Component {
       ) :
       <Text> None </Text>;
   }
-  render() {
-    const toggleImage = () => {
-      this.setState({
-        imageShowing: !this.state.imageShowing,
-      });
-    };
-    let eventPictureSource = this.state.imageSource;
-    if (this.state.event) {
-      if (this.state.event.eventPictureUri) {
-        eventPictureSource = { uri: this.state.event.eventPictureUri };
-      } else if (this.state.event.vibe) {
-        eventPictureSource = vibes[this.state.event.vibe].src;
-      }
-    }
+  generateEventDetails() {
     return (
-      <View style={[styles.imageContainer, { margin: this.defaultMargin }]}>
-        {this.state.imageShowing ?
-          <TouchableOpacity onPress={toggleImage} >
-            <Image
-              source={this.state.event && this.state.event.vibe ?
-                vibes[this.state.event.vibe].src :
-                this.state.imageSource}
-              style={{ width: width - (2 * this.defaultMargin) }}
-            />
-          </TouchableOpacity> :
-          <TouchableOpacity onPress={toggleImage} >
-            <Text style = {styles.standardText }>Users Invited:</Text>
-            {this.getInvitedUserPics(this.state.event)}
-            <Text style = {styles.standardText }>Groups Invited:</Text>
-            {this.getInvitedGroupPics(this.state.event)}
-            <Text style = {styles.standardText }>Vibe: {this.state.event.vibe}</Text>
-            <Text style = {styles.standardText }>Where: {this.state.event.location}</Text>
-          </TouchableOpacity>
-        }
+      <View style={[{ margin: this.defaultMargin, flexDirection: 'column', flex: 1, height: 200 }]}>
+        <Text style = {styles.standardText }>Users Invited:</Text>
+        {this.getInvitedUserPics(this.state.event)}
+        <Text style = {styles.standardText }>Groups Invited:</Text>
+        {this.getInvitedGroupPics(this.state.event)}
+        <Text style = {styles.standardText }>Vibe: {this.state.event.vibe}</Text>
+        <Text style = {styles.standardText }>Where: {this.state.event.location}</Text>
       </View>
+    );
+  }
+  generatePhotoSlides() {
+    return (
+      <View style={[{ margin: this.defaultMargin, flexDirection: 'column', flex: 1, height: 200 }]}>
+        <Image
+          source={ this.state.imageSource }
+          style={{ width: width - (2 * this.defaultMargin) }}
+        />
+      </View>
+    );
+  }
+  render() {
+    return (
+      <Swiper style={styles.wrapper} height={200}>
+        <View>
+          { this.generateEventDetails() }
+        </View>
+        { this.generatePhotoSlides() }
+      </Swiper>
     );
   }
 }
