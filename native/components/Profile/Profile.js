27f1165d73740c8f15b1a3dcdb31f85ece224d79
Elliot Plant <elliotaplant@gmail.com>
2016-03-31 15:16:13 -0700

    Begin styling profile page

--- native/components/Profile/Profile.js
+++ native/components/Profile/Profile.js
@@ -2,10 +2,12 @@
 import React, { Alert, View, Text, TouchableOpacity, Image } from 'react-native';
 import { store } from '../../sharedNative/reducers/reducers.js';
 import NavBar from '../Shared/NavBar.js';
+import BackgroundImage from '../Shared/BackgroundImage.js';
 import EditUser from './EditUser';
 import { navToFull } from '../Shared/NavHelpers';
 import styles from '../../styles/styles.js';
 import { backButton, editButton } from '../Shared/Buttons';
+
 import CirclePic from '../Shared/CirclePic';
 const actions = require('../../sharedNative/actions/actions');
 const profPic = require('../../sharedNative/images/dino-profile.jpeg');
@@ -37,36 +39,37 @@ const Profile = (props) => {
     });
   };
   return (
-    <View>
-    <NavBar
-      title={ 'Profile' }
-      leftButton={backButton}
-      rightButton={editButton(EditUser, props.user, updateUser)}
-    />
-    <View>
-      <CirclePic source={ { uri: props.user.profilePictureUri }} />
-      <View style={styles.listEntryView}>
-        <Text style={styles.group}>Username: { props.user.userName }</Text>
-      </View>
-      <View style={styles.listEntryView}>
-        <Text style={styles.group}>
-          Default Location: {props.user.defaultLocation || 'None'}
-        </Text>
-      </View>
-      <View style={styles.listEntryView}>
-        <Text style={styles.group}>
-          Default Vibe: {props.user.defaultVibe || 'None'}
-        </Text>
-      </View>
-    </View>
-    <TouchableOpacity onPress={() => store.dispatch(actions.logout())} >
-      <View style={styles.listEntryView}>
-        <Text style={{ bottom: 0, color: 'red' }}>
-          Logout
-        </Text>
-      </View>
-    </TouchableOpacity>
-  </View>
+      <BackgroundImage source={require('../../static/bg.jpg')}>
+        <NavBar
+          title={ 'Profile' }
+          leftButton={backButton}
+          rightButton={editButton(EditUser, props.user, updateUser)}
+        />
+        <View style={styles.centerContainerNoMargin}>
+          <CirclePic size={200} source={ { uri: props.user.profilePictureUri }} />
+          <View style={{ height: 20 }} />
+          <View style={styles.listEntryView}>
+            <Text style={styles.standardText}>Username: { props.user.userName }</Text>
+          </View>
+          <View style={styles.listEntryView}>
+            <Text style={styles.standardText}>
+              Default Location: {props.user.defaultLocation || 'None'}
+            </Text>
+          </View>
+          <View style={styles.listEntryView}>
+            <Text style={styles.standardText}>
+              Default Vibe: {props.user.defaultVibe || 'None'}
+            </Text>
+          </View>
+          <TouchableOpacity onPress={() => store.dispatch(actions.logout())} >
+            <View style={styles.listEntryView}>
+              <Text style={{ bottom: 0, color: 'red' }}>
+                Logout
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </BackgroundImage>
   );
 };
 
