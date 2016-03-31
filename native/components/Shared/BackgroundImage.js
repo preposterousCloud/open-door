8cecdc19800d91ecb5dc15f0ddf4e84ef198a71a
Elliot Plant <elliotaplant@gmail.com>
2016-03-31 15:30:11 -0700

    Merge

--- native/components/Shared/BackgroundImage.js
+++ native/components/Shared/BackgroundImage.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import React, { Image } from 'react-native';
 
 export const BackgroundImage = ({ source, children, style, ...props }) => {
   source = source || require('../../static/bg.jpg')
-=======
-import React, {
-  Image,
-} from 'react-native';
-
-module.exports = BackgroundImage = ({ source, children, style, ...props }) => {
->>>>>>> 47450ae4fd80e82f5236c942fb00616b73af5f43
   return (
       <Image
         source={source}
