fd928f25317ea8f3e564635a617dbea7abcbbb66
Elliot Plant <elliotaplant@gmail.com>
2016-03-31 22:21:20 -0700

    Format login page

--- native/components/Login.js
+++ native/components/Login.js
@@ -23,6 +23,7 @@ import styles from '../styles/styles';
 const actions = require('../sharedNative/actions/actions');
 const nameText = require('../ios/opendoor/opendoortext.png');
 const logo = require('../ios/opendoor/opendoorlogo.png');
+import { BackgroundImage } from './Shared/BackgroundImage.js';
 
 const { height, width } = Dimensions.get('window');
 
@@ -102,55 +103,64 @@ const Login = class Login extends React.Component {
   }
   render() {
     return (
-      <ScrollView scrollEnabled={false} >
-        <StatusBar barStyle="light-content" />
-        <View style={styles.centerContainerNoMargin}>
-          <Image source={nameText} style={{ margin: 10, marginTop: 20, height: 70, width: 250 }} />
-          <TextInput
-            autoCapitalize={'none'}
-            autoCorrect={false}
-            maxLength={16}
-            placeholder={'Phone Number (optional)'}
-            value={this.state.phone}
-            style={styles.userInput}
-            keyboardType={'number-pad'}
-            returnKeyType={'next'}
-            onSubmitEditing={() => this.refs.userName.focus()}
-            onChangeText={(text) => this.updateFormProp('phone', text)}
-          />
-          <TextInput
-            ref={'userName'}
-            autoCapitalize={'none'}
-            autoCorrect={false}
-            maxLength={16}
-            placeholder={'User Name (required)'}
-            value={this.state.userName}
-            style={styles.userInput}
-            returnKeyType={'next'}
-            onSubmitEditing={() => this.refs.password.focus()}
-            onChangeText={(text) => this.updateFormProp('userName', text)}
-          />
-          <TextInput
-            ref={'password'}
-            autoCapitalize={'none'}
-            autoCorrect={false}
-            maxLength={16}
-            placeholder={'Password (required)'}
-            value={this.state.password}
-            style={styles.userInput}
-            returnKeyType={'go'}
-            onChangeText={(text) => this.updateFormProp('password', text)}
-            onSubmitEditing={this.loginToApp}
-            secureTextEntry
-          />
-          <View style={{ justifyContent: 'space-around', flexDirection: 'row' }}>
-            <Button text={'Login'} onClick={this.loginToApp} />
-            <View style={{ width: 40 }} />
-            <Button text={'Signup'} onClick={this.signupUser} />
+      <BackgroundImage source={require('../static/bgLibrary/orangewhite.png')}>
+        <ScrollView scrollEnabled={false} >
+          <StatusBar barStyle="light-content" />
+          <View style={styles.center}>
+            <Image source={nameText} style={styles.loginTextLogo} />
+            <View style={styles.underlined}>
+              <TextInput
+                autoCapitalize={'none'}
+                autoCorrect={false}
+                maxLength={16}
+                placeholder={'Phone Number (optional)'}
+                value={this.state.phone}
+                style={styles.userInput}
+                keyboardType={'number-pad'}
+                returnKeyType={'next'}
+                onSubmitEditing={() => this.refs.userName.focus()}
+                onChangeText={(text) => this.updateFormProp('phone', text)}
+              />
+            </View>
+            <View style={styles.underlined}>
+              <TextInput
+                ref={'userName'}
+                autoCapitalize={'none'}
+                autoCorrect={false}
+                maxLength={16}
+                placeholder={'User Name (required)'}
+                value={this.state.userName}
+                style={styles.userInput}
+                returnKeyType={'next'}
+                onSubmitEditing={() => this.refs.password.focus()}
+                onChangeText={(text) => this.updateFormProp('userName', text)}
+              />
+            </View>
+            <View style={styles.underlined}>
+              <TextInput
+                ref={'password'}
+                autoCapitalize={'none'}
+                autoCorrect={false}
+                maxLength={16}
+                placeholder={'Password (required)'}
+                value={this.state.password}
+                style={styles.userInput}
+                returnKeyType={'go'}
+                onChangeText={(text) => this.updateFormProp('password', text)}
+                onSubmitEditing={this.loginToApp}
+                secureTextEntry
+              />
+            </View>
+            <View style={styles.topBuffer} />
+            <View style={{ justifyContent: 'space-around', flexDirection: 'row' }}>
+              <Button text={'Login'} onClick={this.loginToApp} />
+              <View style={{ width: 40 }} />
+              <Button text={'Signup'} onClick={this.signupUser} />
+            </View>
+            <Image source={logo} style={{ marginTop: 40, height: 150, width: 150 }} />
           </View>
-          <Image source={logo} style={{ marginTop: 40, height: 150, width: 150 }} />
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </BackgroundImage>
     );
   }
 };
