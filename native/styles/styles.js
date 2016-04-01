fabc8acd118730badc5ef7f4f67a5a3bef9c20b7
Elliot Plant <elliotaplant@gmail.com>
2016-04-01 09:13:29 -0700

    Display event photo as background on edit event

--- native/styles/styles.js
+++ native/styles/styles.js
@@ -190,7 +190,7 @@ module.exports = StyleSheet.create({
     color: 'white',
   },
   underlined: {
-    borderBottomWidth: 2,
+    borderBottomWidth: 1,
     borderBottomColor: 'white',
   },
   checkboxEmpty: {
@@ -292,6 +292,9 @@ module.exports = StyleSheet.create({
   // text
   bold: {
     fontWeight: '800',
+    fontSize: 20,
+  },
+  large: {
     fontSize: 20,
   },
   white: {
