e71402a54045def7cca6f31a17e8ed8c17fc8b12
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-03-31 17:51:22 -0700

    Enable stylesheet passing in BackgroundImage

--- native/components/Shared/BackgroundImage.js
+++ native/components/Shared/BackgroundImage.js
@@ -5,7 +5,7 @@ export const BackgroundImage = ({ source, children, style, ...props }) => {
   return (
       <Image
         source={source}
-        style={{flex: 1, width: null, height: null, ...style}}
+        style={[{flex: 1, width: null, height: null}, style]}
         {...props}>
         {children}
       </Image>
