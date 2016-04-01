4240442df478b5e7e7749561d81812f4f9598245
Elliot Plant <elliotaplant@gmail.com>
2016-03-31 21:11:58 -0700

    Update general stylized text input

--- native/components/Shared/StyledTextInput.js
+++ native/components/Shared/StyledTextInput.js
@@ -6,24 +6,34 @@ import styles from '../../styles/styles.js';
 
 // things it shoudl be passed:
   // onChangeText, placeholder
+  // defaultValue={props.}
 const StyledTextInput = (props) => (
   <View>
     <TextInput
       autoCapitalize={'none'}
       autoCorrect={false}
-      maxLength={25}
-      placeholder={props.placeholder}
+      maxLength={props.maxLength || 25}
+      placeholder={props.placeholder || ''}
+      value={props.value || undefined}
       style={styles.userInput}
-      returnKeyType={'go'}
-      onChangeText={props.onChangeText}
+      returnKeyType={props.returnKeyType || 'go'}
+      onChangeText={props.onChangeText || (() => null)}
+      keyboardType={props.keyboardType || 'default'}
+      onSubmitEditing={props.onSubmitEditing}
+      secureTextEntry={props.secureTextEntry || false}
     />
   </View>
 );
 
-
 StyledTextInput.propTypes = {
   onChangeText: React.PropTypes.func.isRequired,
+  onSubmitEditing: React.PropTypes.func,
   placeholder: React.PropTypes.string,
+  maxLength: React.PropTypes.number,
+  value: React.PropTypes.string,
+  returnKeyType: React.PropTypes.string,
+  keyboardType: React.PropTypes.string,
+  secureTextEntry: React.PropTypes.bool,
 };
 
 module.exports = StyledTextInput;
