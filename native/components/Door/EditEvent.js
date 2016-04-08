9c5b162e24da54a12cf0cb2dc137441bc09b6f3b
Elliot Plant <elliotaplant@gmail.com>
2016-04-08 14:38:42 -0700

    Make edit event use new Checklist component

--- native/components/Door/EditEvent.js
+++ native/components/Door/EditEvent.js
@@ -4,7 +4,7 @@ import { store } from '../../sharedNative/reducers/reducers.js';
 import { Button } from '../Shared/Button';
 import { BackgroundImage } from '../Shared/BackgroundImage.js';
 import { backButton, cancelButtonNav, cancelButton } from '../Shared/Buttons.js';
-import { navToFull, popScene } from '../Shared/NavHelpers.js';
+import { navTo, navToFull, popScene } from '../Shared/NavHelpers.js';
 import { getTruthies } from '../Shared/HelperFunctions.js';
 import { GroupList, UserList } from '../Shared/StatefulSelectList';
 import * as actions from '../../sharedNative/actions/actions';
@@ -17,42 +17,18 @@ import SelectProfilePic from '../Profile/SelectProfilePic';
 import styles from '../../styles/styles.js';
 import Checklist from '../Shared/Checklist';
 
-const InviteSelects = (props) => {
-  return (
-  <View>
-    <NavBar title={props.route.title} leftButton={backButton} />
-    <props.route.listComponent
-      onItemClick={props.route.onItemClick}
-      preSelected={props.route.preSelected}
-    />
-  </View>
-);
-}
-InviteSelects.propTypes = { route: React.PropTypes.object };
-
-const getInvited = (invitedArr) => {
-  return invitedArr.reduce((allInvitedItems, singleInvitedItem) => {
-    allInvitedItems[singleInvitedItem.id] = true;
-    return allInvitedItems;
-  }, {});
-};
+const getIds = arrOfObjsWithIds => arrOfObjsWithIds.map(obj => obj.id);
 
 class EditEvent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       onSubmit: props.route.onSubmit,
-      event: {
-        invitedFriends: [],
-        invitedGroups: [],
-        preSelectedFriends: [],
-        preSelectedGroups: [],
-      },
+      invitedGroups: [],
+      invitedFriends: [],
     };
     this.navToGroups = this.navToGroups.bind(this);
     this.navToFriends = this.navToFriends.bind(this);
-    this.toggleInviteGroup = this.toggleInviteGroup.bind(this);
-    this.toggleInviteFriend = this.toggleInviteFriend.bind(this);
     this.navToEventPhoto = this.navToEventPhoto.bind(this);
   }
   componentDidMount() {
@@ -60,61 +36,42 @@ class EditEvent extends React.Component {
       if (this.state) {
         api.getEvent(this.props.route.event.id, store.getState().jwt)
         .then((event) => {
-          return this.setState({
-            event: {
-              ...event,
-              invitedFriends: getInvited(event.Users),
-              invitedGroups: getInvited(event.Groups),
-              preSelectedFriends: getInvited(event.Users),
-              preSelectedGroups: getInvited(event.Groups),
-            }
-          });
-        })
-        .then(() => this.forceUpdate());
+          const invitedFriends = getIds(event.Users);
+          const invitedGroups = getIds(event.Groups);
+          return this.setState({ event, invitedGroups, invitedFriends });
+        });
       }
     } else {
       const event = { ...this.state.event, ...this.props.route.event };
       this.setState({ event });
     }
   }
-  toggleInviteFriend(friend) {
-    const event = this.state.event;
-    event.invitedFriends[friend.id] = !event.invitedFriends[friend.id];
-    this.setState({ event });
-  }
-  toggleInviteGroup(group) {
-    const event = this.state.event;
-    event.invitedGroups[group.id] = !event.invitedGroups[group.id];
-    this.setState({ event });
-  }
   navToGroups() {
-    const component = () => (<Checklist
-      preSelected={(user) => user.userName === 'vcipriani'}
-      onSubmit={(checkedArr) => console.log(checkedArr)}
-      data={store.getState().user.friends}
-      displayTextRoute={['userName']}
-    />);
-    navToFull({
-      component,
-      // : Checklist,
-      // preSelected: () => true,
-      // onSubmit: (checkedArr) => console.log(checkedArr),
-      // data: store.getState().user.friends,
-      // displayTextRoute: ['userName'],
-      // title: 'Invite Groups',
-      // listComponent: GroupList,
-      // onItemClick: this.toggleInviteGroup,
-      // preSelected: this.state.event.preSelectedGroups,
-    });
+    navTo(() => (
+      <Checklist
+        preSelected={group => this.state.invitedGroups.indexOf(group.id) > -1}
+        onSubmit={(invitedGroupsFull) => {
+          this.setState({ invitedGroups: getIds(invitedGroupsFull) });
+          popScene();
+        }}
+        data={store.getState().user.Groups}
+        displayTextRoute={['name']}
+      />);
+    )
+    navToFull({ component });
   }
   navToFriends() {
-    navToFull({
-      component: InviteSelects,
-      title: 'Invite Friends',
-      listComponent: UserList,
-      onItemClick: this.toggleInviteFriend,
-      preSelected: this.state.event.preSelectedFriends,
-    });
+    navTo(() => (
+      <Checklist
+        preSelected={friend => this.state.invitedFriends.indexOf(friend.id) > -1}
+        onSubmit={invitedFriendsFull => {
+          this.setState({ invitedFriends: getIds(invitedFriendsFull) });
+          popScene();
+        }}
+        data={store.getState().user.friends}
+        displayTextRoute={['userName']}
+      />)
+    );
   }
   navToEventPhoto() {
     navToFull({
@@ -136,8 +93,8 @@ class EditEvent extends React.Component {
       if (!this.state.event.name) {
         Alert.alert('Your door needs a name!', '', [cancelButton]);
       } else {
-        this.state.event.friends = getTruthies(this.state.event.invitedFriends);
-        this.state.event.groups = getTruthies(this.state.event.invitedGroups);
+        this.state.event.friends = this.state.invitedFriends;
+        this.state.event.groups = this.state.invitedGroups;
         this.state.event.startDateUtc = Date.now();
         this.state.event.endDateUtc = Date.now() + (4 * 3.6e+6); // 4 hours in ms
         if (this.state.event.imageObj) {
