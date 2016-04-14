122e5e5d87d553b47545dcb8d78a265b147d204b
Elliot Plant <elliotaplant@gmail.com>
2016-04-14 16:17:26 -0700

    Removed styling complexity

--- native/components/Shared/UserRow.js
+++ native/components/Shared/UserRow.js
@@ -6,13 +6,13 @@ import { store } from '../../sharedNative/reducers/reducers.js';
 export const UserRow = (props) => {
   const contactMapper = store.getState().contactMap;
   return (
-    <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 10 }}>
-      <View style={{ flexDirection: 'column', flex: 1, alignItems: 'center', marginRight: 20 }}>
+    <View style={styles.listEntryView}>
+      <View style={styles.rowPic}>
         <CirclePic source={{ uri: props.profilePictureUri }}
           size={60}
         />
       </View>
-      <View style={{ flexDirection: 'column', flex: 4, alignItems: 'flex-start' }}>
+      <View style={styles.userRowText}>
         <Text style={styles.mediumText}>{contactMapper[props.id] || props.userName}</Text>
         <Text style={styles.smallText}>{contactMapper[props.id] && props.userName}</Text>
       </View>
